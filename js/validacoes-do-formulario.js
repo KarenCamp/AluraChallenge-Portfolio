7e4ae0e2439c003d384364e3b97ed2e658b55b5d
KarenCamp <karen-campinas@hotmail.com>
2022-03-22 16:47:19 -0300

    manutenção

--- js/validacoes-do-formulario.js
+++ js/validacoes-do-formulario.js
@@ -2,48 +2,54 @@ var inputs = document.querySelectorAll('.input-padrao');
 var formSubmit = document.querySelector(".enviar");
 var formComoUmTodo = document.querySelector("form");
 var regexp = /[0-9]/;
+var modalIntro = document.querySelector('.intro-descontraida');
+var modalTitle = document.querySelector('.modal-title');
+var ulErrorContent = document.querySelector('.error-content');
+var redefinirModalBody = document.querySelector('.redefine-modal-body');
+var btnCloseModal = document.querySelector('.close');
 
-    for(var i = 0; i < inputs.length; i++) {
+
+    for(let i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener('focusout', function() {
         
             if (this.value.length == 0) {
-                confirm(`O campo "${this.name}" não pode estar vazio.`);
                 formSubmit.disabled = true;
                 this.previousElementSibling.dataset.content = " X";
                 this.classList.remove('campo-validado');
                 this.classList.add('campo-invalido');
+                showModalError();
             }
 
-            else if (this.name == "nome" && this.value.match(regexp)) {
-                confirm('Há números no campo "nome".');
+            else if (this.name == "nome" && this.value.match(regexp)){
                 formSubmit.disabled = true;
                 this.previousElementSibling.dataset.content = " X";
                 this.classList.remove('campo-validado');
                 this.classList.add('campo-invalido');
+                showModalError();
             }
         
             else if (this.name == "nome" && this.value.length > 50 || this.name == "nome" && this.value.length < 2 || this.name == "assunto" && this.value.length > 50 || this.name == "assunto" && this.value.length < 2) {
-                confirm(`O campo "${this.name}" deve conter entre 2 a 50 caracteres.`);
                 formSubmit.disabled = true;
                 this.previousElementSibling.dataset.content = " X";
                 this.classList.remove('campo-validado');
                 this.classList.add('campo-invalido');
+                showModalError();
             }
 
             else if (this.name == "e-mail" && this.value.indexOf("@") == -1 || this.name == "e-mail" && this.value.indexOf(".") == -1) {
-                confirm('O campo "e-mail" deve estar em formato de e-mail contendo o caractere especial "@" seguido por um domínio ou provedor seguido por um ponto (.) **Exemplo: text@texto.com**.');
                 formSubmit.disabled = true;
                 this.previousElementSibling.dataset.content = " X";
                 this.classList.remove('campo-validado');
                 this.classList.add('campo-invalido');
+                showModalError();
             }
 
             else if (this.name == "mensagem" && this.value.length < 2 || this.name == "mensagem" && this.value.length > 300) {
-                confirm('O campo "mensagem" deve conter entre 2 a 300 caracteres.');
                 formSubmit.disabled = true;
                 this.previousElementSibling.dataset.content = " X";
                 this.classList.remove('campo-validado');
                 this.classList.add('campo-invalido');
+                showModalError();
             }
         
             else {
@@ -52,17 +58,39 @@ var regexp = /[0-9]/;
                 this.previousElementSibling.dataset.content = " ✔";
             } 
 
+            
             var tdsOsCamposValidados = document.querySelectorAll(".campo-validado");
-            if (tdsOsCamposValidados.length == 4){
-                confirm("Todos os campos estão corretamente preenchidos, formulário pronto para envio.")
+            if (tdsOsCamposValidados.length === 4) {
+                showModalSuccess();
                 formSubmit.disabled = false;
          }
     });
 }
 
+   function showModalError() {
+        modalIntro.innerHTML = "Ops...";
+        modalTitle.innerHTML = "Regras de validações:"
+        ulErrorContent.style.display = "block";
+        redefinirModalBody.style.display = "none";
+        $('#meuPrimeiroModal').modal('show');
+}
+
+   function showModalSuccess() {
+        modalIntro.innerHTML = "Ôba =)";
+        modalTitle.innerHTML = "Sucesso!";
+        ulErrorContent.style.display = "none";
+        redefinirModalBody.style.display = "block";
+        $('#meuPrimeiroModal').modal('show');
+} 
+
+    btnCloseModal.addEventListener('click', function() {
+        $('#meuPrimeiroModal').modal('hide');
+    });
+
+    
     function resetarForm() {
         formComoUmTodo.reset();
         formSubmit.disabled = true;
-    }
+}
 
-    
+
